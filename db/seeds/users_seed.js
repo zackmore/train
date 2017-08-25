353044e1579ae16306cd758f3eb9f9062bd27cb9
zackmore <zackmoredev@gmail.com>
2017-08-25 13:46:41 +0800

    user seed is no more need

--- db/seeds/users_seed.js
+++
@@ -1,8 +0,0 @@
-exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
-      return knex('users').insert([
-        { username: 'asdf', password: 'ee254686131414d7da3f7b862691c6224eed5b0cebd371b73ea353a584b6bbd1' }  // sha256 for 'okayn'
-      ])
-    })
-}
