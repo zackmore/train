5ea14ea3670bdde25f32d71925b07895411c052c
zackmore <zackmoredev@gmail.com>
2017-08-25 11:18:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# A Koa Demo
---
+++ app.js
@@ -0,0 +1,20 @@
+import Koa from 'koa'
+import json from 'koa-json'
+import onerror from 'koa-onerror'
+import logger from 'koa-logger'
+import bodyParser from 'koa-bodyparser'
+import jwt from 'koa-jwt'
+import unless from 'koa-unless'
+
+
+const app = new Koa()
+onerror(app)
+
+app.use(jwt({ secret: '' }).unless({ path: [] }))
+  .use(json())
+  .use(logger())
+  .use(bodyParser())
+
+
+app.listen(3000)
+console.log('Server is starting at port 3000')
